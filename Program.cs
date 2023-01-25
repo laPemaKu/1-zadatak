24ac1b87ab5cf52aaad2152325df216e01772dad
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-01-25 09:19:57 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Napišite program u kojem ćete zatražiti od korisnika da unese niz znakova te će svaki razmak zamijeniti znakom '_'.*/
+            Console.WriteLine("Unesite niz znakovva: ");
+            string niz = Console.ReadLine();
+            niz = niz.Replace(" ","_");
+            Console.Write(niz);
+            Console.ReadKey();
+        }
+    }
+}
